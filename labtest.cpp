31c01c45318e508e5a9511d24c29eedf9ecbc759
nishatsithyt-glitch <nishatsithy.t@gmail.com>
2026-01-06 09:47:01 +0600

    question for labtest

---
+++ labtest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//write a user defined function to find first N prime numbers;
+//the function should be named like : youname_yourid;
+
+/*
+example:
+
+void nishat_90(int n){
+//your code here;
+}
+*/
+
+int main(){
+
+
+    int n;
+    cout<<"Enter the number n"<<endl;
+    cin>>n;
+}
